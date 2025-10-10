334099177b7502aeb84b4ce19ed3e89a28241166
Jakub Kurc <jak.kurc@gmail.com>
2025-10-10 13:03:08 +0200

    Update configuration

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      files = {
+        hidden = true, -- show dotfiles
+        git_ignore = false, -- include gitignored/untracked files
+      },
+    },
+  },
+}
