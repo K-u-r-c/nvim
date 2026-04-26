737fda72482d30ad8186a1dab5997778a3b9ca05
Jakub <58849519+K-u-r-c@users.noreply.github.com>
2026-04-26 22:46:55 +0200

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,14 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, { desc = "LSP Rename" })
+
+local opts = { noremap = true, silent = true, nowait = true }
+
+vim.keymap.set("i", "<M-b>", "<C-o>b", opts)
+vim.keymap.set("i", "<M-f>", "<C-o>w", opts)
+
+vim.keymap.set("n", "<M-b>", "b", opts)
+vim.keymap.set("n", "<M-f>", "w", opts)
+
+vim.keymap.set("v", "<M-b>", "b", opts)
+vim.keymap.set("v", "<M-f>", "w", opts)
